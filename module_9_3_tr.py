8229c93dc4de96b21b0e9a94d1003e418771ddc1
AlexZ-WP <alexz10@mail.ru>
2024-12-21 11:34:25 +0300

    Add files via upload

---
+++ module_9_3_tr.py
@@ -0,0 +1,31 @@
+from module_9.module_9_3 import second
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+'''
+1. В переменную first_result запишите генераторную сборку, которая высчитывает разницу длин строк из 
+списков first и second, если их длины не равны. Для перебора строк попарно из двух списков используйте функцию zip.
+
+
+'''
+first_result = (len(x) - len(y) for x in first for y in second  if len(x) != len(y))
+first_result_1 = ( len(x) - len(y) for x,y  in zip(first,second) if len(x) != len(y))
+first_result_3 = (zip(len(x) - len(y)) for x in first for y in second  if len(x) != len(y))
+
+print(list(first_result))
+print(list(first_result_1))
+print(list(first_result_3))
+'''
+2. В переменную second_result запишите генераторную сборку, которая содержит результаты сравнения длин строк
+в одинаковых позициях из списков first и second. Составьте эту сборку НЕ используя функцию zip. Используйте 
+функции range и len.
+'''
+
+second_result = (len(x) - len(y) for x in first for y in second)
+second_result_1= (len(str(x)) - len(str(y)) for x in range(len(first)) for y in  range(len(second)))
+second_result_2= (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+print(list(second_result))
+print(list(second_result_1))
+print(list(second_result_2))
